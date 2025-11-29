ed1fb586bd03e3e9a56f46063cba6d285136d267
faysil-amin <foodbrochandpur@gmail.com>
2025-11-29 23:04:18 +0600

    add json data

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { ToastContainer } from "react-toastify";
 import Navbar from "./Conastractor/Navbar/Navbar";
 import CoundBoxs from "./Conastractor/CoundBoxs/CoundBoxs";
 import BackgroundColor from "./Conastractor/BackgroundColor/BackgroundColor";
+import Containbox from "./Conastractor/Containbox/Containbox";
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <BackgroundColor>
         <CoundBoxs></CoundBoxs>
+        <Containbox></Containbox>
       </BackgroundColor>
       <ToastContainer />
     </>
---
+++ src/Conastractor/Containbox/Containbox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Containbox = () => {
+  return <div></div>;
+};
+
+export default Containbox;
